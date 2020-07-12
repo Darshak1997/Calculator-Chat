7c0e9546b4ad8d5684002bb508ca39befdb24dd1
Darshak1997 <42878805+Darshak1997@users.noreply.github.com>
2020-07-12 13:10:43 -0500

    make it better12

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const Html5WebSocket = require('html5-websocket');
 const ReconnectingWebSocket = require('reconnecting-websocket');
 let isServerLocal = false;
 let ws_host = 'https://calc-chat-app.herokuapp.com/';
-let ws_port = '80';
+let ws_port = process.env.PORT || 80;
 if (isServerLocal == true) {
     let ws_host = 'localhost';
     let ws_port = '3030';
