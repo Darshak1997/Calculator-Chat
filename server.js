aa3901b30bd9655dd66a2bc3e68041c8458f72c1
Darshak1997 <42878805+Darshak1997@users.noreply.github.com>
2020-07-13 12:25:55 -0500

    app

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const WebSocket = require('ws');
 const { Server } = require('ws');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+console.log(process.env.PORT)
 const INDEX = path.join('/App.js');
 
 const server = express()
